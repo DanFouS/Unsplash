78a73c763cee764a3c3265522f258efc1e3a2dd4
DanFouS <rouzmfawer@gmail.comgit config --global user.email rouzmfawer@gmail.comgit config --global user.email rouzmfawer@gmail.com>
2021-01-20 17:20:33 +0100

    post req

--- splash/src/app/app.component.ts
+++ splash/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 // import { Cloudinary } from module('../../../backend/uploads/cloudinary');
 @Component({
@@ -10,6 +13,7 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   // title = 'splash';
   url = '';
+  images: any;
   constructor(private http: HttpClient) {}
   selectFile(event: any) {
     if (event.target.files) {
@@ -17,14 +21,25 @@ export class AppComponent {
       reader.readAsDataURL(event.target.files[0]);
       reader.onload = (event: any) => {
         this.url = event.target.result;
-        //console.log(this.url);
+        console.log(this.url);
 
-        this.http
-          .post('http://localhost:5000/upload-images', {
-            image: this.url,
-          })
-          .subscribe();
+        // this.http
+        //   .post('http://localhost:5000/upload-images', {
+        //     image: this.url,
+        //   })
+        //   .subscribe(
+        //     (res) => console.log(res),
+        //     (err) => console.log(err)
+        //   );
       };
     }
+  }
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('file', this.images);
+    this.http.post('http://localhost:5000/upload-images', formData).subscribe(
+      (res) => console.log(res),
+      (err) => console.log(err)
+    );
   }
 }
